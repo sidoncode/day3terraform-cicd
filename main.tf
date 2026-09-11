69eae162846eeca82849d22f761196db9ae0cd81
Siddharth Sharma <siddharth54330@gmail.com>
2026-09-11 09:40:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ provider "azurerm" {
   features {}
 }
 
-variable "student_name" {
-  description = "Your unique short nickname, e.g. stu1"
-  type        = string
+# CHANGE THIS to your name, e.g. "stu1", "stu2", "stu3", "stu4"
+locals {
+  student_name = "stu1"
 }
 
 # Random suffix so the storage account name is guaranteed unique
@@ -27,13 +27,13 @@ resource "random_id" "suffix" {
 
 # A resource group just for this student
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${var.student_name}"
+  name     = "rg-${local.student_name}"
   location = "eastus"
 }
 
 # The storage account
 resource "azurerm_storage_account" "sa" {
-  name                     = "sa${var.student_name}${random_id.suffix.hex}"
+  name                     = "sa${local.student_name}${random_id.suffix.hex}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
